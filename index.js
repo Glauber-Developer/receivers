d72039a79797c7aeaaef88b91ca89c558a63d6c1
HulkFront <glauberna@hotmail.com>
2024-05-14 20:14:22 -0300

    fix(index): removing white lines and organization

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
+// Imports
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const MONGODB_URI = 'mongodb+srv://receiver:bHURnypTkEaNszji@cluster0.ukjvtoj.mongodb.net/';
 
 app.use(cors());
 app.use(bodyParser.json());
 
 app.use('/api/receivers', require('./src/routes/receivers'));
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+    });
+}
 
-const mongoose = require('mongoose');
-
-mongoose.connect('mongodb+srv://receiver:bHURnypTkEaNszji@cluster0.ukjvtoj.mongodb.net/', {
+mongoose.connect(MONGODB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}).then(() => console.log('Conexão com o MongoDB estabelecida com sucesso'))
-    .catch((err) => console.error('Erro ao conectar ao MongoDB', err));
+}).then(() => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso');
+}).catch((err) => {
+    console.error('Erro ao conectar ao MongoDB', err);
+});
+
+module.exports = app;
